c15a57b3b0091b229ef5c7fb8a618c8412239b75
Cooper Beltrami <45991010+cooperbeltrami@users.noreply.github.com>
2021-01-04 12:10:57 +1100

    Initial Commit

---
+++ XLSX to CSV Conversion POC/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace XLSX_to_CSV_Conversion_POC
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ConvertBtn
+            // 
+            this.ConvertBtn.Location = new System.Drawing.Point(12, 12);
+            this.ConvertBtn.Name = "ConvertBtn";
+            this.ConvertBtn.Size = new System.Drawing.Size(61, 251);
+            this.ConvertBtn.TabIndex = 0;
+            this.ConvertBtn.Text = "Convert";
+            this.ConvertBtn.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ConvertBtn.UseVisualStyleBackColor = true;
+            this.ConvertBtn.Click += new System.EventHandler(this.ConvertBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(79, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(336, 251);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.WordWrap = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 273);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ConvertBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.Text = "XLSX to CSV Conversion Proof of Concept";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertBtn;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ XLSX to CSV Conversion POC/MainForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XLSX_to_CSV_Conversion_POC
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {
+
+            InitializeComponent();
+        }
+
+        private void ConvertBtn_Click(object sender, EventArgs e)
+        {
+            // Create new open file dialog
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                RestoreDirectory = true,
+                InitialDirectory = Environment.SpecialFolder.Desktop.ToString(),
+                Title = "Browse for XLSX files to convert",
+                DefaultExt = "xlsx",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Multiselect = false, // This can be changed in the future
+            };
+
+            // Once user has chosen and click ok on the file dialog window
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Retreive info from select file
+                string filePath = openFileDialog.FileName;
+
+                using (var fileStream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (var fileReader = ExcelDataReader.ExcelReaderFactory.CreateReader(fileStream))
+                    {
+                        string csvText = "";
+
+                        // Read each line of the file
+                        do
+                        {
+                            int numberOfColumns = fileReader.FieldCount;
+
+                            while (fileReader.Read()) // Each row
+                            {
+                                for (int column = 0; column < numberOfColumns; column++) // Each column 
+                                {
+                                    csvText += fileReader.GetValue(column) + ",";
+                                }
+
+                                csvText += "\n";
+                            }
+                        } while (fileReader.NextResult()); // For each sheet
+
+                        // Print in textbox
+                        textBox1.Text = csvText;
+
+                       string newPath = filePath.Substring(0, filePath.IndexOf(".")) + ".csv";
+                       File.AppendAllText(newPath, csvText);
+                    }
+                }
+            }
+        }
+    }
+}
